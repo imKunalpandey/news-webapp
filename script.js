3648ef66ce3a88fef175eb9e8114cff2278b91d2
imKunalpandey <138514560+imKunalpandey@users.noreply.github.com>
2024-07-16 18:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const url="https://newsapi.org/v2/everything?q=";
 window.addEventListener('load',()=> fetchNews("india"));
 
